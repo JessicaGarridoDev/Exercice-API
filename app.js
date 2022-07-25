e691bfd48236c461ad91fcd2c552f346a592d2fd
JessicaGarridoDev <jessica.garrido.di@gmail.com>
2022-07-25 18:10:37 +0200

    premier test passé : created product successfully

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Application Express 
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser")
 const cors = require("cors");
 
 //Middlewwares
 app.use(cors())
-
+app.use(bodyParser.json())
+  
 //Routes
+
+app.post("/api/products", (req, res) => {
+    const product = req.body
+    res.send({ product : req.body })
+})
 //enregistre « Requête reçue ! » dans la console et passe l'exécution
 app.use((req, res, next) => {
   console.log('Requête reçue !');
